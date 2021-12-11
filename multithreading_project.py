5eb68d0a3d969e3cb2115bfb8ab185cf89c30620
Ben <nicole@Nicoles-MBP.home>
2021-12-11 14:10:50 -0500

    first commit

--- multithreading_project.py
+++ multithreading_project.py
@@ -12,6 +12,9 @@ genres = []
 genre_words = dict()
 
 # functions
+
+# Get all the words from titles of movies given the genre 'genre' and find the
+# 10 most common stemmed nouns in movie titles for that genre.
 def get_words(genre):
     global movies
     genre_words = list()
@@ -20,24 +23,39 @@ def get_words(genre):
 
     for title in titles:
         clean = ''
+        # clean the current title of numbers and punctuation
         for char in title:
             if char.isalpha() or char == ' ':
                 clean += char.lower()
         title = clean
-        raw = word_tokenize(title) # get a list of every word in the sanitized title
-        tokens = nltk.pos_tag(raw) # analyze each word to see what part of speech it is
+
+        # get a list of every word in the sanitized title
+        raw = word_tokenize(title)
+        
+        # analyze each word to see what part of speech it is
+        tokens = nltk.pos_tag(raw)
+
+        # Filter words to only return nouns that are not in the exclude list
         nouns = [x[0] for x in tokens if x[1] == 'NN' or x[1] == 'NNS' or x[1] == 'NNP' or x[1] == 'NNPS' ] # make a new list of the words only if they are nouns
         nouns = [x for x in nouns if x not in exclude_list] # remove exculded words
+        
+        # stem every word to get the root
         stemmer = PorterStemmer()
         nouns = [stemmer.stem(w) for w in nouns]
+
+        # add the sanitized words from the current title to the word list for
+        # this genre
         if len(genre_words) > 0: # add each word of word list to genre_words
             for word in nouns:
                 genre_words.append(word)
         else:
             genre_words = nouns
     
+    # Create a counter object over 'genre_words' to get counts of each word
     c = Counter(genre_words)
-    # get the 10 most common words used in titles for the given genre
+
+    # get the 10 most common words used in titles for the given genre and write
+    # them to a temp file
     most_common = c.most_common(10)
     f = open('answers~.txt', "a")
     f.write("\033[1m" + genre + "\033[0m" + ':\n')
@@ -46,6 +64,8 @@ def get_words(genre):
     f.write('\n')
     f.close()
 
+
+## Actual Program ##
 # read in data
 with open('data/movies.csv', newline = '') as csvfile:
     next(csvfile)
@@ -60,7 +80,7 @@ with open('data/movies.csv', newline = '') as csvfile:
             except:
                 movies[genre] = [title]
 
-
+# Main Thread #
 if __name__ == "__main__":
     # Part 1
     # Among all movies, list the top 10 genres - in terms of number of movies 
@@ -84,6 +104,9 @@ if __name__ == "__main__":
     print('Part 2')
     genres = [k[0] for k in genre_movies]
     processes = list()
+
+    # create each process, append it to the list, start each process, then tell
+    # the rest of the program to wait until the processes are complete
     for key in genres:
         p = multiprocessing.Process(target=get_words, args=(key, ))
         processes.append(p)
@@ -92,9 +115,12 @@ if __name__ == "__main__":
     for p in processes:
         p.join()
 
+    # read results from temp file and print them
     with open('answers~.txt') as f:
         lines = f.readlines()
         for line in lines:
             print(line.rstrip())
+
+    # remove temp file
     if os.path.exists('answers~.txt'):
         os.remove('answers~.txt')
